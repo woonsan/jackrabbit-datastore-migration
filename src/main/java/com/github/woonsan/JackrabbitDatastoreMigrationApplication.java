60d85e74c682e8661022c4e20420754b6e13f332
Woonsan Ko <w.ko@onehippo.com>
2016-09-06 18:44:17 -0400

    initial project creation using spring initlizr (spring-boot)

---
+++ src/main/java/com/github/woonsan/JackrabbitDatastoreMigrationApplication.java
@@ -0,0 +1,12 @@
+package com.github.woonsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackrabbitDatastoreMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackrabbitDatastoreMigrationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/woonsan/JackrabbitDatastoreMigrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.woonsan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JackrabbitDatastoreMigrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
